d26dde24a956d1bcdb251fa78ea1ca5904b04e0b
Roman Anisimov <romakms@yandex.ru>
2019-06-03 11:28:23 +0300

    DEV-003: Rectal praktik

--- Jenkins/merge-master-to-releases.sh
+++ Jenkins/merge-master-to-releases.sh
@@ -4,6 +4,7 @@
 #   To fetch all remote branches, in advanced Git settings, specify Refspec: +refs/heads/*:refs/remotes/origin/*
 #   Add Prune stale remote branches
 #   Add Check out to specific local branch
+# Real talk, think about it.
 
 RELEASES=`git branch -r | fgrep origin/release | sed 's@ *origin/@@'`
 
